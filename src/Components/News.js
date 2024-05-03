c12e2024e529ed45996861b7f80dc232cb2c5c1f
Meghavi02 <meghavilad@gmail.com>
2024-05-03 23:01:50 +0530

    error fix

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,49 +3,15 @@ import Newsitem from "./Newsitem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
+import articles from "../Sampleout.json";
 
 const News = (props) => {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  const [totalResults, setTotalResults] = useState(0);
+  // const [articles, setArticles] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [page, setPage] = useState(1);
+  // const [totalResults, setTotalResults] = useState(0);
 
-  const capitalizeFirstLetter = (string) => {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  };
-
-  const updateNews = async () => {
-    props.setProgress(20);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.contry}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-    setLoading(true);
-    let data = await fetch(url);
-    props.setProgress(30);
-    let parsedData = await data.json();
-    props.setProgress(70);
-    setArticles(parsedData.articles);
-    setTotalResults(parsedData.totalResults);
-    setLoading(false);
-    props.setProgress(100);
-  };
-
-  useEffect(() => {
-    document.title = `${capitalizeFirstLetter(props.category)} - J News`;
-    updateNews();
-    // eslint-disable-next-line
-  }, []);
-
-  const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${
-      props.contry
-    }&category=${props.category}&apiKey=39260496197f414e90d073ef6b7847cf&page=${
-      page + 1
-    }&pageSize=${props.pageSize}`;
-    setPage(page + 1);
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    setArticles(articles.concat(parsedData.articles));
-    setTotalResults(parsedData.totalResults);
-  };
+  console.log(articles);
 
   return (
     <>
@@ -53,36 +19,36 @@ const News = (props) => {
         className="text-center"
         style={{ margin: "35px 0", marginTop: "90px" }}
       >
-        J News - Top {capitalizeFirstLetter(props.category)} Headlines
+        J News - Top Headlines
       </h1>
-      {loading && <Spinner />}
-      <InfiniteScroll
+      {/* {loading && <Spinner />} */}
+      {/* <InfiniteScroll
         dataLength={articles.length}
-        next={fetchMoreData}
+        // next={fetchMoreData}
         hasMore={articles.length !== totalResults}
         loader={<Spinner />}
         style={{ overflow: "hidden" }}
-      >
-        <div className="container">
-          <div className="row">
-            {articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <Newsitem
-                    title={element.title ? element.title : ""}
-                    description={element.description ? element.description : ""}
-                    imgUrl={element.urlToImage}
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-          </div>
+      > */}
+      <div className="container">
+        <div className="row">
+          {articles.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <Newsitem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imgUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
+              </div>
+            );
+          })}
         </div>
-      </InfiniteScroll>
+      </div>
+      {/* </InfiniteScroll> */}
     </>
   );
 };
